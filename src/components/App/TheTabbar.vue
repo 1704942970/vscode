13639c4f713b84299cf58109a851f8d331ae5523
1704942970 <94671239+1704942970@users.noreply.github.com>
2022-04-28 14:20:54 +0800

    Update TheTabbar.vue

--- src/components/App/TheTabbar.vue
+++ src/components/App/TheTabbar.vue
@@ -2,7 +2,7 @@
   <van-tabbar v-model="active" route>
     <van-tabbar-item icon="wap-home-o" :to="{ name: 'Store' }">首页</van-tabbar-item>
     <van-tabbar-item icon="apps-o" :to="{ name: 'Category' }">分类</van-tabbar-item>
-    <van-tabbar-item icon="shopping-cart-o" :to="{ name: 'ShoppingCart' }">购物车</van-tabbar-item>
+    <!-- <van-tabbar-item icon="shopping-cart-o" :to="{ name: 'ShoppingCart' }">购物车</van-tabbar-item>-->
     <van-tabbar-item icon="user-o" :to="{ name: 'UserCenter' }">个人</van-tabbar-item>
   </van-tabbar>
 </template>
