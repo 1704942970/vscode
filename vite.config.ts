f5a34a38bc5c4487e368273f8553ccb53568ee2a
wxh <wxh@chinaums.com>
2022-04-28 10:31:01 +0800

    修改部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default ({ mode }) => {
         '@': path.resolve(__dirname, './src'),
       },
     },
-    base: mode === 'development' ? '' : '/up/',
+    base: mode === 'development' ? '' : '',
     plugins: [
       vue(),
       vueJsx(),
